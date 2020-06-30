c25a29a5d6a15dcf59d07aef0d32c3df9ee546e9
JaeseongPark <qkrwotjd1445@naver.com>
2020-06-30 11:00:07 +0900

    init

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const logger = require('morgan')
+const app = express()
+let users = [
+    {id: 1, name: 'Alice'},
+    {id: 2, name: 'Bek'},
+    {id: 3, name: 'Chris'}
+]
+
+app.get('/', (req, res) => res.send('Heelo World!'))
+app.get('/users', (req, res) => {
+    req.query.limit = req.query.limit || 10
+    const limit = parseInt(req.query.limit, 10)
+
+    if (Number.isNaN(limit)) {
+        res.status(400).end();
+    } else {
+        res.json(users.slice(0, limit))
+    }
+})
+app.get('/users/:id', (req, res) => {
+    const id = parseInt(req.params.id, 10)
+    if (Number.isNaN(id)) {
+        return res.status(400).end()
+    }
+
+    const user = users.filter(user => user.id === id)[0]
+    if (!user) {
+        return res.status(404).end()
+    }
+    res.json(user)
+})
+app.delete('/users/:id', (req, res) => {
+    const id = parseInt(req.params.id, 10)
+    if (Number.isNaN(id)) {
+        return res.status(400).end()
+    }
+
+    users = users.filter(user => user.id !== id)
+    res.status(204).end()
+})
+ 
+module.exports = app;
